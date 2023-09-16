7acbf6ceeef584dedbe883fc42721a2cc19c965b
henriquerojo <henriquerojo10@gmail.com>
2023-09-16 01:21:03 -0300

    início dia-4

---
+++ dia-4/script.js
@@ -0,0 +1,23 @@
+// estruturas condicionais
+/*
+modelo estrutural de um if
+
+if (condição que retorna true ou false) 
+{
+    ação dependendo do retorno da condição
+}
+
+existe também o else e o else if, o que pode caracterizar uma estrutura condicional composta ou aninhada
+*/
+
+let condicao = true;
+
+if (condicao)
+{
+    console.log('entrou no if')
+}
+else
+{
+    console.log('entrou no else')
+}
+
