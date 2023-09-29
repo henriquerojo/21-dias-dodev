9906ec64929482d5367d2a8444df3a67f09d5f6f
henriquerojo <henriquerojo10@gmail.com>
2023-09-29 20:43:47 -0300

    criando classe computador

---
+++ dia-13/exercicio1.js
@@ -0,0 +1,47 @@
+/*
+
+1 - Crie uma classe para representar o seu computador:
+
+
+Tipo - desktop ou notebook;
+Processador - nome do seu processador, ex: Ryzen 5
+Video - Integrado ou Dedicado;
+Armazenamento - número em GB de memória, ex: 500 ou 1000;
+MemoriaRam - número em GB de memória, ex: 4, 8 ou 16;
+SSD - True ou False
+
+
+2 - Implemente um método nessa classe para exibir as informações no console;
+
+
+   Se você não souber todas essas informações do seu computador, não tem problema preencha com os 
+   valores de exemplo mesmo.
+
+*/
+
+class Computador
+{
+    // propriedades
+    tipo;
+    processador;
+    video;
+    armazenamento;
+    memoriaram;
+    ssd;
+    // metodos construtores
+    constructor(tipo, processador, video, armazenamento, memoriaram, ssd)
+    {
+        this.tipo = tipo;
+        this.processador = processador;
+        this.video = video;
+        this.armazenamento = armazenamento;
+        this.memoriaram = memoriaram;
+        this.ssd = ssd;
+    }
+    // metodos da classe
+    informacoes()
+    {
+        console.log('Tipo: ' + this.tipo + '\nProcessador: ' + this.processador + '\nVideo: ' + this.video + '\nArmazenamento: ' + this.armazenamento + '\nMemória RAM: ' + this.memoriaram + '\nSSD: ' + this.ssd);
+    }
+}
+
---
+++ dia-13/exercicio2.js
@@ -0,0 +1,43 @@
+/*
+
+1 - Crie uma classe Carro com as seguintes propriedades:
+
+
+Nome - qual a equipe do carro Ferrari, Mercedes, McLaren, etc;
+Potencia - número de cavalos de potência do carro;
+VelocidadeMaxima - qual a maior velocidade que o carro pode alcançar;
+Aceleracao - o tempo em segundos que o carro leva para ir de 0 a 100 km/h
+
+
+2 - Adicione na classe carro um método que recebe como parâmetro uma distância em metros e calcula 
+o tempo em segundos para percorrer essa distância. Use a seguinte fórmula para o cálculo: 
+
+
+ resultado = distância / (VelocidadeMaxima / Aceleracao). 
+
+
+
+   A função deve retornar esse resultado;
+
+3 - Crie uma classe Corrida com as seguintes propriedades:
+
+
+Nome - nome do local da corrida;
+Tipo - uma corrida pode ser Fórmula 1, Stock Car, Rally, etc;
+Distancia - o total em metros da corrida;
+Participantes - um array de objetos da classe Carro
+Vencedor - qual a equipe que ganhou a corrida;
+
+4 - Adicione na classe Corrida um método que verifica qual foi o carro que completou a corrida em menor 
+tempo, para isso utilize o método criado na classe carro. Salve o nome do carro que fez o menor tempo na 
+propriedade “Vencedor”.
+
+
+5 - Adicione na classe Corrida um método que exibe na tela quem é o vencedor da corrida.
+
+
+6 - Crie um objeto da classe Corrida e preencha a propriedade Participantes com alguns objetos da calsse Carro.
+
+
+7 - Chame o método para verificar quem fez o menor tempo e por fim o método que exibe o vencedor.
+*/
