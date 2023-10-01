72a2c70d6c40c1c48e0143b1f51162ea0d01d879
henriquerojo <henriquerojo10@gmail.com>
2023-10-01 18:36:34 -0300

    finalização exercicio 1, criando classe computador e populando array de computadores, exibindo todas as posições da array em um laço for consumindo o método da classe computador para exibir as informações

--- dia-13/exercicio1.js
+++ dia-13/exercicio1.js
@@ -43,5 +43,53 @@ class Computador
     {
         console.log('Tipo: ' + this.tipo + '\nProcessador: ' + this.processador + '\nVideo: ' + this.video + '\nArmazenamento: ' + this.armazenamento + '\nMemória RAM: ' + this.memoriaram + '\nSSD: ' + this.ssd);
     }
-} 
+}
 
+let continuar = true;
+let indiceComputador = 0;
+let computadores = [];
+
+while (continuar) 
+{
+    let tipo = prompt('Digite o tipo do seu computador: ');
+    let processador = prompt('Digite o processador do seu computador: ');
+    let video = prompt('Digite qual o tipo de GPU do seu computador: ');
+    let armazenamento = prompt('Digite o armazenamento você possui em seu computador (GB): ');
+    let memoriaram = prompt('Digite a quantidade de memória RAM que você possui em seu computador (GB): ');
+    let ssd = prompt('Você possui SSD? [S|N]: ');
+    ssd = ssd.toUpperCase();
+    while (ssd != "S" && ssd != "N") 
+    {
+        ssd = prompt('Você possui SSD?[S|N]: ');
+        ssd = ssd.toUpperCase();    
+    }
+    if (ssd == 'S')
+    {
+        ssd = true;
+    }
+    else
+    {
+        ssd = false;
+    }
+
+    let computador = new Computador(tipo, processador, video, armazenamento, memoriaram, ssd);
+    computadores[indiceComputador] = computador;
+
+    let pergunta = prompt('Quer continuar? [S|N]: ');
+    pergunta = pergunta.toUpperCase();
+    while (pergunta != 'S' && pergunta != 'N') 
+    {
+        pergunta = prompt('Digite "S" para SIM ou "N" para NÃO\nQuer continuar?[S|N]: ');
+    }
+    if (pergunta == 'N')
+    {
+        continuar = false;
+        continue;
+    }
+    indiceComputador ++;
+}
+
+for (let c = 0; c < computadores.length; c++) 
+{
+    computadores[c].informacoes();    
+}
