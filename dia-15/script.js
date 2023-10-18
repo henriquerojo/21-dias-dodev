64c00acee3a47042e1a8d439283e1bb75d47ac96
henriquerojo <henriquerojo10@gmail.com>
2023-10-18 00:04:19 -0300

    criação pastas e enunciados aula 15 e 16 para fazer durante curso de node.js

---
+++ dia-15/script.js
@@ -0,0 +1,31 @@
+/*
+
+1 - Crie dois arrays: arrayA e arrayB. Preencha os dois arrays com 10 números cada, pode declarar já com valores;
+  let arrayA = [13,20,10,22,24,20,27,17,19,14]
+
+  let arrayB = [24,22,23,11,15,17,29,14,16,21]
+
+
+
+2 - Implemente uma função que receba dois arrays como parâmetro. Essa função deve retornar um array que 
+contenha apenas os números em comum nos dois arrays recebidos como parâmetro.
+
+
+3 - Se um número se repetir apenas no mesmo array, ele NÃO deve ser adicionado como um número comum.
+
+
+4 - No array de saída NÃO deve ter números repetidos
+
+Exemplo: 
+
+
+
+Entrada: arrayA = [13,20,10,22,24,20,27,17,19,14]
+
+         arrayB = [24,22,23,11,15,17,29,14,16,21]
+
+
+
+Saída:   numerosEmComum = [22,24,17,14]
+
+*/
